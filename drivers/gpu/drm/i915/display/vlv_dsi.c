f2a2a312a5e882c464dadd4022f022c043325f66
Hans de Goede <hdegoede@redhat.com>
2021-12-18 11:41:58 +0100

    drm/i915/vlv_dsi: Add DMI quirk for wrong panel modeline in BIOS on Asus TF103C (v2)
    Vtotal is wrong in the BIOS supplied modeline for the DSI panel on
    the Asus TF103C leading to the last line of the display being shown
    as the first line.
    
    Original: "1280x800": 60 67700 1280 1312 1328 1376 800 808 812 820 0x8 0xa
    Fixed:    "1280x800": 60 67700 1280 1312 1328 1376 800 808 812 816 0x8 0xa
    
    The factory installed Android has a hardcoded modeline in its kernel,
    causing it to not suffer from this BIOS bug;
    and the Android boot-splash which uses the EFI FB which does have this bug
    has the last line all black causing the bug to not be visible.
    
    This commit introduces a generic DMI based quirk mechanism to vlv_dsi for
    doing various fixups, and uses this to correct the modeline.
    
    v2:
    - s/mode_fixup/dmi_quirk/ to make the new DMI quirk mechanism more generic
    - Add a comment with the old and new modelines to the patch and commit msg
    
    Reviewed-by: Javier Martinez Canillas <javierm@redhat.com>
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>

--- drivers/gpu/drm/i915/display/vlv_dsi.c
+++ drivers/gpu/drm/i915/display/vlv_dsi.c
@@ -23,6 +23,7 @@
  * Author: Jani Nikula <jani.nikula@intel.com>
  */
 
+#include <linux/dmi.h>
 #include <linux/slab.h>
 
 #include <drm/drm_atomic_helper.h>
@@ -1827,6 +1828,36 @@ static void vlv_dphy_param_init(struct intel_dsi *intel_dsi)
 	intel_dsi_log_params(intel_dsi);
 }
 
+typedef void (*vlv_dsi_dmi_quirk_func)(struct intel_dsi *intel_dsi,
+				       struct drm_display_mode *fixed_mode);
+
+/*
+ * Vtotal is wrong on the Asus TF103C leading to the last line of the display
+ * being shown as the first line. The factory installed Android has a hardcoded
+ * modeline, causing it to not suffer from this BIOS bug.
+ *
+ * Original mode: "1280x800": 60 67700 1280 1312 1328 1376 800 808 812 820 0x8 0xa
+ * Fixed    mode: "1280x800": 60 67700 1280 1312 1328 1376 800 808 812 816 0x8 0xa
+ */
+static void vlv_dsi_asus_tf103c_mode_fixup(struct intel_dsi *intel_dsi,
+					   struct drm_display_mode *fixed_mode)
+{
+	if (fixed_mode->vtotal == 820)
+		fixed_mode->vtotal -= 4;
+}
+
+static const struct dmi_system_id vlv_dsi_dmi_quirk_table[] = {
+	{
+		/* Asus Transformer Pad TF103C */
+		.matches = {
+			DMI_MATCH(DMI_SYS_VENDOR, "ASUSTeK COMPUTER INC."),
+			DMI_MATCH(DMI_PRODUCT_NAME, "TF103C"),
+		},
+		.driver_data = (void *)vlv_dsi_asus_tf103c_mode_fixup,
+	},
+	{ }
+};
+
 void vlv_dsi_init(struct drm_i915_private *dev_priv)
 {
 	struct drm_device *dev = &dev_priv->drm;
@@ -1836,6 +1867,7 @@ void vlv_dsi_init(struct drm_i915_private *dev_priv)
 	struct intel_connector *intel_connector;
 	struct drm_connector *connector;
 	struct drm_display_mode *current_mode, *fixed_mode;
+	const struct dmi_system_id *dmi_id;
 	enum port port;
 	enum pipe pipe;
 
@@ -1962,6 +1994,14 @@ void vlv_dsi_init(struct drm_i915_private *dev_priv)
 	if (!fixed_mode) {
 		drm_dbg_kms(&dev_priv->drm, "no fixed mode\n");
 		goto err_cleanup_connector;
+	}
+
+	dmi_id = dmi_first_match(vlv_dsi_dmi_quirk_table);
+	if (dmi_id) {
+		vlv_dsi_dmi_quirk_func quirk_func =
+			(vlv_dsi_dmi_quirk_func)dmi_id->driver_data;
+
+		quirk_func(intel_dsi, fixed_mode);
 	}
 
 	intel_panel_init(&intel_connector->panel, fixed_mode, NULL);
