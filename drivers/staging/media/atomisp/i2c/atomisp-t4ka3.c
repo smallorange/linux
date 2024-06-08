f3c15e858695d484a1e32559b2684f014603e3b5
Hans de Goede <hdegoede@redhat.com>
2024-06-08 13:32:57 +0200

    media: atomisp: t4ka3: Update copyright headers
    Switch to SPDX copyright headers and add myself as co-author.
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>

--- drivers/staging/media/atomisp/i2c/atomisp-t4ka3.c
+++ drivers/staging/media/atomisp/i2c/atomisp-t4ka3.c
@@ -1,17 +1,10 @@
+// SPDX-License-Identifier: GPL-2.0
 /*
  * Support for T4KA3 8M camera sensor.
  *
- * Copyright (c) 2015 Intel Corporation. All Rights Reserved.
+ * Copyright (C) 2015 Intel Corporation. All Rights Reserved.
  * Copyright (C) 2016 XiaoMi, Inc.
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License version
- * 2 as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * Copyright (C) 2024 Hans de Goede <hansg@kernel.org>
  */
 
 #include <linux/bitops.h>
