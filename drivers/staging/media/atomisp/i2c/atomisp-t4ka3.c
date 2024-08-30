02e8621dbc616204058dbc2570e2a144edf0b289
Kate Hsuan <hpa@redhat.com>
2024-08-30 15:15:23 +0800

    media: atomisp: t4ka3: Accessing regiter through CCI helper
    The regiter read/write operations was replaced with the CCI helper.
    
    Signed-off-by: Kate Hsuan <hpa@redhat.com>

--- drivers/staging/media/atomisp/i2c/atomisp-t4ka3.c
+++ drivers/staging/media/atomisp/i2c/atomisp-t4ka3.c
@@ -24,6 +24,7 @@
 #include <linux/module.h>
 #include <linux/moduleparam.h>
 #include <linux/pm_runtime.h>
+#include <linux/regmap.h>
 #include <linux/string.h>
 #include <linux/slab.h>
 #include <linux/types.h>
@@ -33,6 +34,19 @@
 #include <linux/acpi.h>
 
 #include "t4ka3.h"
+
+#define T4KA3_REG_PRODUCT_ID_HIGH		CCI_REG8(0x0000)
+#define T4KA3_REG_PRODUCT_ID_LOW		CCI_REG8(0x0001)
+
+#define T4KA3_REG_IMG_ORIENTATION		CCI_REG8(0x0101)
+#define T4KA3_REG_COARSE_INTEGRATION_TIME	CCI_REG16(0x0202)
+#define T4KA3_REG_DIGGAIN_GREEN_R		CCI_REG16(0x020e)
+#define T4KA3_REG_DIGGAIN_RED			CCI_REG16(0x0210)
+#define T4KA3_REG_DIGGAIN_BLUE			CCI_REG16(0x0212)
+#define T4KA3_REG_DIGGAIN_GREEN_B		CCI_REG16(0x0214)
+#define T4KA3_REG_GLOBAL_GAIN			CCI_REG16(0x0234)
+#define T4KA3_REG_FRAME_LENGTH_LINES		CCI_REG16(0x0340)
+#define T4KA3_REG_TEST_PATTERN_MODE		CCI_REG8(0x0601)
 
 static inline struct t4ka3_device *ctrl_to_t4ka3(struct v4l2_ctrl *ctrl)
 {
@@ -47,246 +61,7 @@ static const int t4ka3_hv_flip_bayer_order[] = {
 	MEDIA_BUS_FMT_SGBRG10_1X10,
 };
 
-static int t4ka3_detect(struct i2c_client *client, u16 *id);
-
-static int t4ka3_read_reg(struct i2c_client *client, u16 len,
-						u16 reg, u16 *val)
-{
-	struct i2c_msg msg[2];
-	u16 data[T4KA3_SHORT_MAX] = {0};
-	int err, i;
-	int retry_cnt = 5;
-
-	if (len > T4KA3_BYTE_MAX) {
-		dev_err(&client->dev,
-			"%s error, invalid data length\n", __func__);
-		return -EINVAL;
-	}
-
-	memset(msg, 0 , sizeof(msg));
-
-	msg[0].addr = client->addr;
-	/*msg[0].addr = 0x37;*/
-	msg[0].flags = 0;
-	msg[0].len = I2C_MSG_LENGTH;
-	msg[0].buf = (u8 *)data;
-	/* high byte goes first */
-	data[0] = cpu_to_be16(reg);
-
-	msg[1].addr = client->addr;
-	/*msg[1].addr = 0x37;*/
-	msg[1].len = len;
-	msg[1].flags = I2C_M_RD;
-	msg[1].buf = (u8 *)data;
-
-	while (retry_cnt-- > 0) {
-		err = i2c_transfer(client->adapter, msg, 2);
-		if (err != 2) {
-			if (err >= 0)
-				err = -EIO;
-
-			if (retry_cnt <= 0)
-				goto error;
-		} else
-			break;
-	}
-
-	/* high byte comes first */
-	if (len == T4KA3_8BIT)
-		*val = (u8)data[0];
-	else {
-		/* 16-bit access is default when len > 1 */
-		for (i = 0; i < (len >> 1); i++)
-			val[i] = be16_to_cpu(data[i]);
-	}
-
-	return 0;
-
-error:
-	dev_err(&client->dev, "read from offset 0x%x error %d", reg, err);
-	return err;
-}
-
-static int t4ka3_i2c_write(struct i2c_client *client, u16 len, u8 *data)
-{
-	struct i2c_msg msg;
-	const int num_msg = 1;
-	int ret;
-
-	msg.addr = client->addr;
-	/*msg.addr = 0x37;*/
-
-	msg.flags = 0;
-	msg.len = len;
-	msg.buf = data;
-	ret = i2c_transfer(client->adapter, &msg, 1);
-
-	if (ret != num_msg)
-		dev_err(&client->dev, "%s error!! ret = %d\n", __func__, ret);
-
-	return ret == num_msg ? 0 : -EIO;
-}
-
-static int t4ka3_write_reg(struct i2c_client *client, u16 data_length,
-							u16 reg, u16 val)
-{
-	int ret;
-	unsigned char data[4] = {0};
-	u16 *wreg = (u16 *)data;
-	const u16 len = data_length + sizeof(u16); /* 16-bit address + data */
-	int retry_cnt = 5;
-
-	if (data_length != T4KA3_8BIT && data_length != T4KA3_16BIT) {
-		dev_err(&client->dev,
-			"%s error, invalid data_length\n", __func__);
-		return -EINVAL;
-	}
-
-	/* high byte goes out first */
-	*wreg = cpu_to_be16(reg);
-
-	if (data_length == T4KA3_8BIT) {
-		data[2] = (u8)(val);
-	} else {
-		/* T4KA3_16BIT */
-		u16 *wdata = (u16 *)&data[2];
-		*wdata = cpu_to_be16(val);
-	}
-
-	while (retry_cnt-- > 0) {
-		ret = t4ka3_i2c_write(client, len, data);
-		if (ret) {
-
-		} else
-			break;
-	}
-
-	return ret;
-}
-
-/*
- * t4ka3_write_reg_array - Initializes a list of T4KA3 registers
- * @client: i2c driver client structure
- * @reglist: list of registers to be written
- *
- * This function initializes a list of registers. When consecutive addresses
- * are found in a row on the list, this function creates a buffer and sends
- * consecutive data in a single i2c_transfer().
- *
- * __t4ka3_flush_reg_array, __t4ka3_buf_reg_array() and
- * __t4ka3_write_reg_is_consecutive() are internal functions to
- * t4ka3_write_reg_array_fast() and should be not used anywhere else.
- *
- */
-
-static int __t4ka3_flush_reg_array(struct i2c_client *client,
-				     struct t4ka3_write_ctrl *ctrl)
-{
-	u16 size;
-
-	if (ctrl->index == 0)
-		return 0;
-
-	size = sizeof(u16) + ctrl->index; /* 16-bit address + data */
-	ctrl->buffer.addr = cpu_to_be16(ctrl->buffer.addr);
-	ctrl->index = 0;
-
-	return t4ka3_i2c_write(client, size, (u8 *)&ctrl->buffer);
-}
-
-static int __t4ka3_buf_reg_array(struct i2c_client *client,
-				   struct t4ka3_write_ctrl *ctrl,
-				   const struct t4ka3_reg *next)
-{
-	int size;
-	u16 *data16;
-
-	switch (next->type) {
-	case T4KA3_8BIT:
-		size = 1;
-		ctrl->buffer.data[ctrl->index] = (u8)next->val;
-		break;
-	case T4KA3_16BIT:
-		size = 2;
-		data16 = (u16 *)&ctrl->buffer.data[ctrl->index];
-		*data16 = cpu_to_be16((u16)next->val);
-		break;
-	default:
-		return -EINVAL;
-	}
-
-	/* When first item is added, we need to store its starting address */
-	if (ctrl->index == 0)
-		ctrl->buffer.addr = next->sreg;
-
-	ctrl->index += size;
-
-	/*
-	 * Buffer cannot guarantee free space for u32? Better flush it to avoid
-	 * possible lack of memory for next item.
-	 */
-	if (ctrl->index + sizeof(u16) >= T4KA3_MAX_WRITE_BUF_SIZE)
-		return __t4ka3_flush_reg_array(client, ctrl);
-
-	return 0;
-}
-
-static int __t4ka3_write_reg_is_consecutive(struct i2c_client *client,
-				   struct t4ka3_write_ctrl *ctrl,
-				   const struct t4ka3_reg *next)
-{
-	if (ctrl->index == 0)
-		return 1;
-	return ctrl->buffer.addr + ctrl->index == next->sreg;
-}
-
-static int t4ka3_write_reg_array(struct i2c_client *client,
-			const struct t4ka3_reg *reglist)
-{
-	const struct t4ka3_reg *next = reglist;
-	struct t4ka3_write_ctrl ctrl;
-	int err;
-
-	ctrl.index = 0;
-	for (; next->type != T4KA3_TOK_TERM; next++) {
-		switch (next->type & T4KA3_TOK_MASK) {
-		case T4KA3_TOK_DELAY:
-			err = __t4ka3_flush_reg_array(client, &ctrl);
-			if (err) {
-				dev_err(&client->dev,
-					"%s: write error\n", __func__);
-			return err;
-			}
-			msleep(next->val);
-			break;
-		default:
-			/*
-			* If next address is not consecutive, data needs to be
-			* flushed before proceed.
-			*/
-			if (!__t4ka3_write_reg_is_consecutive(client, &ctrl,
-						next)) {
-
-			err = __t4ka3_flush_reg_array(client, &ctrl);
-			if (err) {
-				dev_err(&client->dev,
-					"%s@%d: write error\n", __func__, __LINE__);
-				return err;
-				}
-			}
-			err = __t4ka3_buf_reg_array(client, &ctrl, next);
-			if (err) {
-				dev_err(&client->dev,
-					"%s@%d: write error\n", __func__, __LINE__);
-				return err;
-			}
-			break;
-		}
-	}
-
-
-	return __t4ka3_flush_reg_array(client, &ctrl);
-}
+static int t4ka3_detect(struct t4ka3_device *client, u16 *id);
 
 static void t4ka3_set_bayer_order(struct t4ka3_device *sensor,
 				  struct v4l2_mbus_framefmt *fmt)
@@ -375,24 +150,17 @@ unlock:
 /* Horizontal flip the image. */
 static int t4ka3_t_hflip(struct v4l2_subdev *sd, int value)
 {
-	struct i2c_client *c = v4l2_get_subdevdata(sd);
 	struct t4ka3_device *sensor = to_t4ka3_sensor(sd);
 	int ret;
-	u16 val;
+	u64 val;
 
 	if (sensor->streaming)
 		return -EBUSY;
 
-	ret = t4ka3_read_reg(c, T4KA3_8BIT,
-				T4KA3_REG_IMG_ORIENTATION, &val);
-	if (ret)
-		return ret;
-	if (value)
-		val |= T4KA3_HFLIP_BIT;
-	else
-		val &= ~T4KA3_HFLIP_BIT;
-	ret = t4ka3_write_reg(c, T4KA3_8BIT,
-				T4KA3_REG_IMG_ORIENTATION, val);
+	val = value ? T4KA3_HFLIP_BIT : 0;
+
+	ret = cci_update_bits(sensor->regmap, T4KA3_REG_IMG_ORIENTATION,
+			      T4KA3_HFLIP_BIT, val, NULL);
 	if (ret)
 		return ret;
 
@@ -405,24 +173,17 @@ static int t4ka3_t_hflip(struct v4l2_subdev *sd, int value)
 /* Vertically flip the image */
 static int t4ka3_t_vflip(struct v4l2_subdev *sd, int value)
 {
-	struct i2c_client *c = v4l2_get_subdevdata(sd);
 	struct t4ka3_device *sensor = to_t4ka3_sensor(sd);
 	int ret;
-	u16 val;
+	u64 val;
 
 	if (sensor->streaming)
 		return -EBUSY;
 
-	ret = t4ka3_read_reg(c, T4KA3_8BIT,
-				T4KA3_REG_IMG_ORIENTATION, &val);
-	if (ret)
-		return ret;
-	if (value)
-		val |= T4KA3_VFLIP_BIT;
-	else
-		val &= ~T4KA3_VFLIP_BIT;
-	ret = t4ka3_write_reg(c, T4KA3_8BIT,
-				T4KA3_REG_IMG_ORIENTATION, val);
+	val = value ? T4KA3_VFLIP_BIT : 0;
+
+	ret = cci_update_bits(sensor->regmap, T4KA3_REG_IMG_ORIENTATION,
+			      T4KA3_VFLIP_BIT, val, NULL);
 	if (ret)
 		return ret;
 
@@ -432,19 +193,15 @@ static int t4ka3_t_vflip(struct v4l2_subdev *sd, int value)
 	return 0;
 }
 
-static int t4ka3_test_pattern(struct v4l2_subdev *sd, s32 value)
+static int t4ka3_test_pattern(struct t4ka3_device *sensor, s32 value)
 {
-	struct i2c_client *client = v4l2_get_subdevdata(sd);
-
-	return t4ka3_write_reg(client, T4KA3_8BIT,
-			T4KA3_REG_TEST_PATTERN_MODE, value);
+	return cci_write(sensor->regmap, T4KA3_REG_TEST_PATTERN_MODE, value, NULL);
 }
 
 static long __t4ka3_set_exposure(struct v4l2_subdev *sd,
 					u16 coarse_itg,
 					u16 gain, u16 digitalgain)
 {
-	struct i2c_client *client = v4l2_get_subdevdata(sd);
 	int ret;
 	u16 lines_per_frame;
 	struct t4ka3_device *dev = to_t4ka3_sensor(sd);
@@ -465,39 +222,33 @@ static long __t4ka3_set_exposure(struct v4l2_subdev *sd,
 	else
 		lines_per_frame = T4KA3_LINES_PER_FRAME;
 
-	ret = t4ka3_write_reg(client, T4KA3_16BIT,
-				T4KA3_REG_FRAME_LENGTH_LINES,
-				lines_per_frame);
+	ret = cci_write(dev->regmap, T4KA3_REG_FRAME_LENGTH_LINES, lines_per_frame, NULL);
 	if (ret)
 		goto out_disable;
 
 	/* set exposure gain */
-	ret = t4ka3_write_reg(client, T4KA3_16BIT,
-				T4KA3_REG_COARSE_INTEGRATION_TIME,
-				coarse_itg);
+	ret = cci_write(dev->regmap, T4KA3_REG_COARSE_INTEGRATION_TIME, coarse_itg, NULL);
 	if (ret)
 		goto out_disable;
 
 	/* set analogue gain */
-	ret = t4ka3_write_reg(client, T4KA3_16BIT,
-					T4KA3_REG_GLOBAL_GAIN, gain);
+	ret = cci_write(dev->regmap, T4KA3_REG_GLOBAL_GAIN, gain, NULL);
 	if (ret)
 		goto out_disable;
 	/* set digital gain*/
-	ret = t4ka3_write_reg(client, T4KA3_16BIT,
-					T4KA3_REG_DIGGAIN_GREEN_R, digitalgain);
+	ret = cci_write(dev->regmap, T4KA3_REG_DIGGAIN_GREEN_R, digitalgain, NULL);
 	if (ret)
 		goto out_disable;
-	ret = t4ka3_write_reg(client, T4KA3_16BIT,
-					T4KA3_REG_DIGGAIN_RED, digitalgain);
+
+	ret = cci_write(dev->regmap, T4KA3_REG_DIGGAIN_RED, digitalgain, NULL);
 	if (ret)
 		goto out_disable;
-	ret = t4ka3_write_reg(client, T4KA3_16BIT,
-					T4KA3_REG_DIGGAIN_BLUE, digitalgain);
+
+	ret = cci_write(dev->regmap, T4KA3_REG_DIGGAIN_BLUE, digitalgain, NULL);
 	if (ret)
 		goto out_disable;
-	ret = t4ka3_write_reg(client, T4KA3_16BIT,
-					T4KA3_REG_DIGGAIN_GREEN_B, digitalgain);
+
+	ret = cci_write(dev->regmap, T4KA3_REG_DIGGAIN_GREEN_B, digitalgain, NULL);
 	if (ret)
 		goto out_disable;
 
@@ -547,25 +298,24 @@ static long t4ka3_ioctl(struct v4l2_subdev *sd,
 	return 0;
 }
 
-static int t4ka3_detect(struct i2c_client *client, u16 *id)
+static int t4ka3_detect(struct t4ka3_device *sensor, u16 *id)
 {
+	struct i2c_client *client = v4l2_get_subdevdata(&sensor->sd);
 	struct i2c_adapter *adapter = client->adapter;
-	u16 high, low;
+	u64 high, low;
 
 	/* i2c check */
 	if (!i2c_check_functionality(adapter, I2C_FUNC_I2C))
 		return -ENODEV;
 
 	/* check sensor chip ID	 */
-	if (t4ka3_read_reg(client, T4KA3_8BIT, T4KA3_REG_PRODUCT_ID,
-			     &high)) {
-		dev_err(&client->dev, "sensor_id_high = 0x%x\n", high);
+	if (cci_read(sensor->regmap, T4KA3_REG_PRODUCT_ID_HIGH, &high, NULL)) {
+		dev_err(&client->dev, "sensor_id_high = 0x%llx\n", high);
 		return -ENODEV;
 	}
 
-	if (t4ka3_read_reg(client, T4KA3_8BIT, T4KA3_REG_PRODUCT_ID + 1,
-			     &low)) {
-		dev_err(&client->dev, "sensor_id_low = 0x%x\n", low);
+	if (cci_read(sensor->regmap, T4KA3_REG_PRODUCT_ID_LOW, &low, NULL)) {
+		dev_err(&client->dev, "sensor_id_low = 0x%llx\n", low);
 		return -ENODEV;
 	}
 
@@ -596,7 +346,7 @@ t4ka3_s_config(struct v4l2_subdev *sd, int irq)
 		return ret;
 	}
 
-	ret = t4ka3_detect(client, &sensor_id);
+	ret = t4ka3_detect(dev, &sensor_id);
 	if (ret) {
 		dev_err(&client->dev, "Failed to detect sensor.\n");
 		goto fail_detect;
@@ -631,7 +381,7 @@ static int t4ka3_s_ctrl(struct v4l2_ctrl *ctrl)
 	case V4L2_CID_TEST_PATTERN:
 		dev_dbg(&client->dev, "%s: V4L2_CID_TEST_PATTERN: %d\n",
 			__func__, ctrl->val);
-		ret = t4ka3_test_pattern(&sensor->sd, ctrl->val);
+		ret = t4ka3_test_pattern(sensor, ctrl->val);
 		break;
 	case V4L2_CID_VFLIP:
 		dev_dbg(&client->dev, "%s: V4L2_CID_VFLIP: %d\n",
@@ -646,12 +396,12 @@ static int t4ka3_s_ctrl(struct v4l2_ctrl *ctrl)
 	case V4L2_CID_VBLANK:
 		dev_dbg(&client->dev, "%s: V4L2_CID_VBLANK: %d\n",
 			__func__, ctrl->val);
-		ret = t4ka3_write_reg(client, T4KA3_16BIT, T4KA3_REG_FRAME_LENGTH_LINES,
-				      sensor->format.height + ctrl->val);
+		ret = cci_write(sensor->regmap, T4KA3_REG_FRAME_LENGTH_LINES,
+				sensor->format.height + ctrl->val, NULL);
 		break;
 	case V4L2_CID_EXPOSURE:
-		ret = t4ka3_write_reg(client, T4KA3_16BIT, T4KA3_REG_COARSE_INTEGRATION_TIME,
-				      ctrl->val);
+		ret = cci_write(sensor->regmap, T4KA3_REG_COARSE_INTEGRATION_TIME,
+				ctrl->val, NULL);
 		break;
 	default:
 		ret = -EINVAL;
@@ -684,16 +434,18 @@ static int t4ka3_s_stream(struct v4l2_subdev *sd, int enable)
 			goto error_unlock;
 		}
 
-		ret = t4ka3_write_reg_array(client, t4ka3_init_config);
+		ret = cci_multi_reg_write(sensor->regmap, t4ka3_init_config,
+					  ARRAY_SIZE(t4ka3_init_config), NULL);
 		if (ret)
 			goto error_powerdown;
 
 		/* enable group hold */
-		ret = t4ka3_write_reg_array(client, t4ka3_param_hold);
+		ret = cci_multi_reg_write(sensor->regmap, t4ka3_param_hold,
+					  ARRAY_SIZE(t4ka3_param_hold), NULL);
 		if (ret)
 			goto error_powerdown;
 
-		ret = t4ka3_write_reg_array(client, sensor->res->regs);
+		ret = cci_multi_reg_write(sensor->regmap, sensor->res->regs, sensor->res->regs_len, NULL);
 		if (ret)
 			goto error_powerdown;
 
@@ -703,12 +455,13 @@ static int t4ka3_s_stream(struct v4l2_subdev *sd, int enable)
 			goto error_powerdown;
 
 		/* disable group hold */
-		ret = t4ka3_write_reg_array(client, t4ka3_param_update);
+		ret = cci_multi_reg_write(sensor->regmap, t4ka3_param_update,
+					  ARRAY_SIZE(t4ka3_param_update), NULL);
 		if (ret)
 			goto error_powerdown;
 
-		ret = t4ka3_write_reg_array(client,
-					    t4ka3_streaming);
+		ret = cci_multi_reg_write(sensor->regmap, t4ka3_streaming,
+					  ARRAY_SIZE(t4ka3_streaming), NULL);
 		if (ret) {
 			dev_err (&client->dev, "Error on setting reg\n");
 			goto error_powerdown;
@@ -716,9 +469,8 @@ static int t4ka3_s_stream(struct v4l2_subdev *sd, int enable)
 
 		sensor->streaming = 1;
 	} else {
-
-		ret = t4ka3_write_reg_array(client,
-					    t4ka3_suspend);
+		ret = cci_multi_reg_write(sensor->regmap, t4ka3_suspend,
+					  ARRAY_SIZE(t4ka3_suspend), NULL);
 		if (ret) {
 			dev_err(&client->dev, "Error on writing streaming config\n");
 			goto error_powerdown;
@@ -936,7 +688,7 @@ static int t4ka3_pm_resume(struct device *dev)
 	/* waiting for the sensor after powering up */
 	msleep(20);
 
-	ret = t4ka3_detect(client, &sensor_id);
+	ret = t4ka3_detect(sensor, &sensor_id);
 	
 	if (ret) {
 		dev_err(&client->dev, "sensor detect failed\n");
@@ -989,6 +741,11 @@ static int t4ka3_probe(struct i2c_client *client)
 	pm_runtime_enable(&client->dev);
 	pm_runtime_set_autosuspend_delay(&client->dev, 1000);
 	pm_runtime_use_autosuspend(&client->dev);
+
+	dev->regmap = devm_cci_regmap_init_i2c(client, 16);
+	if (IS_ERR(dev->regmap))
+		return PTR_ERR(dev->regmap);
+	dev->dev = &client->dev;
 
 	ret = t4ka3_s_config(&dev->sd, client->irq);
 	if (ret)
